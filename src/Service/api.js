6f55883d14842941039f488a70fafa89a9554753
Mathivanan Marimuthu <35862356+jackmathi@users.noreply.github.com>
2024-07-22 17:24:04 +0530

    service-floder-added

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,11 +1,9 @@
 import data from "./CustomerData";
-
 // Data Fetch  Custome hook Function
 export const usefetch = async () => {
-    return new Promise((res) => {
-      setTimeout(() => {
-        res(data);
-      }, 600);
-    });
-  }
-  
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(data);
+    }, 600);
+  });
+};
