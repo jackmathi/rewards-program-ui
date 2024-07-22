59b07ebc51ec5ada5583b837e3b0901c839a35cf
Mathivanan Marimuthu <35862356+jackmathi@users.noreply.github.com>
2024-07-22 18:20:29 +0530

    Delete src/Components/common directory

--- src/Components/common/api.js
+++
@@ -1,9 +0,0 @@
-import data from "./CustomerData";
-// Data Fetch  Custome hook Function
-export const usefetch = async () => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      res(data);
-    }, 600);
-  });
-};
